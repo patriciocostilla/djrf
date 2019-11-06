60e895cfdea822c316b0bc71dffab3dc8c97f01c
volderpayoda <volderpayoda@gmail.com>
2019-11-06 11:05:21 -0300

    Adding user views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,6 +1,7 @@
 from rest_framework import generics
 from .models import Snippet
-from .serializers import SnippetSerializer
+from django.contrib.auth.models import User
+from .serializers import SnippetSerializer, UserSerializer
 
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):
@@ -10,3 +11,11 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
