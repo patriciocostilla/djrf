4c8ee929db6a133213c2a5ce02732187b01bd35f
volderpayoda <volderpayoda@gmail.com>
2019-11-06 11:06:46 -0300

    Adding user views to urls.py

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,7 +4,11 @@ from snippets import views
 
 urlpatterns = [
     path('snippets/', views.SnippetList.as_view()),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view())
+    # Ex: /snippets/5
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+    path('users/', views.UserList.as_view()),
+    # Ex: /users/3
+    path('users/<int:pk>/', views.UserDetail.as_view())
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
