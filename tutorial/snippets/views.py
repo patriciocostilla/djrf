32878624c582b8251fea744141449b077bdfc661
patriciocostilla <patriciocostilla@gmail.com>
2019-11-07 20:27:16 -0300

    Updating permissions in snippet views

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, permissions
 from .models import Snippet
 from django.contrib.auth.models import User
 from .serializers import SnippetSerializer, UserSerializer
+from .permissions import IsOwnerOrReadOnly
 
 # Create your views here.
 class SnippetList(generics.ListCreateAPIView):
@@ -15,7 +16,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly)
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
