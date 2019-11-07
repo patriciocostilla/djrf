322e2a45edf5f3cf7f66ef40a0ed1b600010e111
patriciocostilla <patriciocostilla@gmail.com>
2019-11-07 20:17:18 -0300

    Adding security to endpoints

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, permissions
 from .models import Snippet
 from django.contrib.auth.models import User
 from .serializers import SnippetSerializer, UserSerializer
@@ -7,6 +7,7 @@ from .serializers import SnippetSerializer, UserSerializer
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
@@ -14,6 +15,7 @@ class SnippetList(generics.ListCreateAPIView):
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly, )
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
